c0167a7a733d4cd41ec2a63111cd90ae6307d2d8
Michael Giordano <MLGIORDANO1@gmail.com>
2017-07-13 11:39:31 -0400

    small changes, setting up R portion of sim

---
+++ Primary_simulation_code.R
@@ -0,0 +1,10 @@
+library("MplusAutomation")
+
+# Run the data generating files
+runModels("C:\\Users\\mgiordan\\git\\msem_sim\\genData\\genData_i012_n1000_norm.inp")
+
+# Generate MPLUS Scripts to Fit models
+
+# Fit MPLUS models
+
+
